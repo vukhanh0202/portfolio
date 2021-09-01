2816136b69e39c9b1750a4bd71667023a687745a
Vu Khanh Nguyen <vukhanh.nguyen@citynow.vn>
2021-09-01 18:00:22 +0700

    add header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import React from 'react';
 import './App.css';
+import Header from './components/header';
 import Home from './pages/home';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Home />
     </div>
   );
---
+++ src/components/header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.scss';
+
+function Header() {
+    return (
+        <div className="header">
+            Header
+        </div>
+    );
+}
+
+export default Header;
