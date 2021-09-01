52ea864e640e2b455064d17b7e6901ff4179e210
Vu Khanh Nguyen <vukhanh.nguyen@citynow.vn>
2021-09-01 15:14:18 +0700

    Deploy vercel app

---
+++ src/pages/home/components/SectionIntroduce/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.scss';
+
+function SectionIntroduce() {
+    return (
+        <div className="section-introduce">
+        </div>
+    );
+}
+
+export default SectionIntroduce;
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SectionIntroduce from './components/SectionIntroduce';
+import './styles.scss';
+
+function Home() {
+    return (
+        <div>
+            <SectionIntroduce/>
+        </div>
+    );
+}
+
+export default Home;
