7a6e42b36fb2a656ae005291d6c60e289c1a773e
Vu Khanh Nguyen <vukhanh.nguyen@citynow.vn>
2021-09-09 10:52:14 +0700

    Update delay animation

--- src/pages/home/components/SectionIntroduce/index.jsx
+++ src/pages/home/components/SectionIntroduce/index.jsx
@@ -9,25 +9,23 @@ function SectionIntroduce() {
             <div className="section-introduce container">
                 <Row>
                     <Col span={15} className="introduce">
-                        <h2 className="title wow fadeInUp center" data-wow-delay="3s">HI THERE, I'M</h2>
+                        <h2 className="title wow fadeInUp center" data-wow-delay="2s">HI THERE, I'M</h2>
                         <div className="content">
-                            <div className="content__primary  wow fadeInUp center" data-wow-delay="3s">
+                            <div className="content__primary  wow fadeInUp center" data-wow-delay="2s">
                                 Nguyen Vu <br />Khanh
                             </div>
-                            <div className="content__sub  wow fadeInUp center" data-wow-delay="3s">
+                            <div className="content__sub  wow fadeInUp center" data-wow-delay="2s">
                                 SOFTWARE ENGINEER (WEB DEVELOPER)
                             </div>
-                            <div className="content__description  wow fadeInUp center" data-wow-delay="3s">
+                            <div className="content__description  wow fadeInUp center" data-wow-delay="2s">
                                 Experienced software engineer with a demonstrated history of working in the information technology and services industry
                             </div>
                         </div>
-                        <div className="action wow rollIn center" data-wow-delay="3s">
+                        <div className="action wow rollIn center" data-wow-delay="2s">
                             <ButtonPrimary />
                         </div>
                     </Col>
                     <Col span={8} offset={1} className="image">
-                        {/* <img className="img" alt="back and white" src={imgss} />
-                        <img className="img-sub" alt="back and white" src={imgss} /> */}
                     </Col>
                 </Row>
             </div>
